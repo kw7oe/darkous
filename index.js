65819254429f42b54405eed38741ed5c4764d6c9
kw7oe <choongkwern@hotmail.com>
2017-05-08 14:08:29 +0800

    Change base url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // API EndPoint
-var baseURL =  "https://testing-api-1.herokuapp.com/weather/"
+var baseURL =  "https://random-shit-api.com/weather/"
 
 // Get Element
 var locationField = document.getElementById("location-input");
